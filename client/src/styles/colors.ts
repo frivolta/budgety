c52cf80bdd636559324b198818fa8975bcbf9fde
frivolta <rivoltafilippo@gmail.com>
2020-09-10 10:05:16 +0200

    Styling, colors

---
+++ client/src/styles/colors.ts
@@ -0,0 +1,20 @@
+export const blue = {
+  200: "#007AFF",
+};
+
+export const purple = {
+  200: "#6F52ED",
+};
+
+export const violet = {
+  200: "#6F52ED",
+};
+
+export const neutral = {
+  100: "#FFFFFF",
+  200: "#F8F9FE",
+  300: "#EBEFF7",
+  400: "#D0D1D2",
+  500: "#484848",
+  600: "#000000",
+};
