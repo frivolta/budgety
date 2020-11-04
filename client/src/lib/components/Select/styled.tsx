98fb0f69cb912d2a3fb2c3d7d980e83455f05425
frivolta <rivoltafilippo@gmail.com>
2020-11-04 12:08:29 +0100

    Styling expenses

--- client/src/lib/components/Select/styled.tsx
+++ client/src/lib/components/Select/styled.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import arrowDownElement from "./assets/images/arrow-down.svg";
 
 export const SelectField = styled.select`
   -webkit-appearance: none;
@@ -7,10 +8,23 @@ export const SelectField = styled.select`
   margin: 14px 0px;
   width: 100%;
   font-size: 16px;
-  border: 1px solid ${(props) => props.theme.colors.primary};
-  background: url(http://cdn1.iconfinder.com/data/icons/cc_mono_icon_set/blacks/16x16/br_down.png)
-    no-repeat right #ddd;
+  padding: 14px;
+  border: 1px solid ${(props) => props.theme.colors.text};
+  background-color: ${(props) => props.theme.colors.componentBackground};
   background-position-x: 95%;
   border-radius: ${(props) => props.theme.borderRadius.base};
   height: 50px;
+  outline: none;
+  background-image: url(${arrowDownElement});
+  background-repeat: no-repeat;
+  background-position: calc(100% - 16px);
+  background-size: 16px;
+  color: ${(props) => props.theme.colors.lightText};
+  ::placeholder {
+    color: ${(props) => props.theme.colors.placeholder};
+  }
+  :focus {
+    border: 1px solid ${(props) => props.theme.colors.primary};
+    transition: all 0.5s;
+  }
 `;
